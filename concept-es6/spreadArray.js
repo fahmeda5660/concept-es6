21b9c22c882da496dde985cbe2560a80f2f20767
fahmeda5660 <fariha.208860@gmail.com>
2023-02-22 02:17:31 +0600

    first commit

---
+++ concept-es6/spreadArray.js
@@ -0,0 +1,45 @@
+// const number = [2, 4, 1, 9, 3, 5, 26, 7];
+// const number2 = [32, 45, 67];
+// console.log(Math.min(...number));
+
+// // const newArr = [123,21,24,...number2, 99];
+
+// // console.log(newArr)
+// // const x = 5;
+// // let y =x;
+// // y = 8
+
+// // console.log(x)
+// // console.log(y)
+
+
+
+// const arr1 = [1,2,3,4,5];
+
+// const arr2 = [...arr1];
+
+// arr1.push(99);
+// arr1.push(100);
+
+// arr2.push(55)
+
+
+// console.log(arr1)
+// console.log(arr2)
+
+
+// let array1 = [1,2,3,4,5,6,7];
+
+// let first = array1[0];
+// let second = array1[1];
+
+// let [first,second,...mehedy] = array1;
+
+
+// console.log(first,second)
+// console.log(mehedy)
+// console.log(array1)
+
+
+
+
